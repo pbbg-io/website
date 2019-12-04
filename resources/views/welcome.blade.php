ff5ad972a71b4a164c418ad7719f290121144387
Ian <hi@ian.pe>
2019-12-04 07:51:15 +0000

    Remove auth from pbbg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,26 +65,13 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     pbbg.io
                 </div>
 
                 <div class="links">
+                    <a href="https://titan.pbbg.io">Titan Game Engine</a>
                     <a href="https://github.com/pbbg-io">Github</a>
                     <a href="https://pbbg.chat">Discord</a>
                     <a href="https://github.com/1e4/idlescape">IdleScape Enhancement Suite</a>
