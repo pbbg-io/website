340c07c90ead8d42d5776505297288e0d5470f49
Ian <hi@ian.pe>
2019-12-12 20:39:36 +0000

    Fix the extension seeder

--- database/seeds/ExtensionSeeder.php
+++ database/seeds/ExtensionSeeder.php
@@ -15,14 +15,19 @@ class ExtensionSeeder extends Seeder
         $faker = Faker\Factory::create();
 
         for($i=0;$i<10;$i++) {
+            $name = strtolower($faker->firstName()) . '/' . \Str::kebab(implode(" ", $faker->words(2)));
+
+            $display_name = \Str::title(str_replace(['/', '-'], ' ', $name));
+
             $extension = new \App\Extensions();
-            $extension->name = $faker->words(1, true);
+            $extension->name = $display_name;
             $extension->author = 1;
             $extension->description = $faker->paragraphs(3, true);
             $extension->rating = mt_rand(1,5);
             $extension->version = 'dev';
             $extension->ratings = mt_rand(0,300);
             $extension->installs = mt_rand(1200, 34000);
+            $extension->namespace = $name;
             $extension->save();
         }
     }
