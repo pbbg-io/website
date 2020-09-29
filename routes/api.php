972c63c14a82289cedf461ca8ab5c94dc1f466c6
Ian <hello@ian.pe>
2020-09-29 15:33:56 +0100

    fix api for lb

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,53 @@ Route::get('/version', 'Api\VersionController@index');
 
 Route::post('/github/webhook/release', 'Api\GithubWebhook@release');
 
+Route::post('/github', function () {
+
+    $pass = false;
+
+    if (request()->get('action', 'open') === 'closed') {
+        $pass = true;
+    } else {
+        $pass = false;
+    }
+
+
+    if ($pr = request()->get('pull_request')) {
+        if (isset($pr['merged']) && $pr['merged'] === true) {
+            $pass = true;
+            echo "Merged is true\n";
+        }
+    } else {
+        $pass = false;
+    }
+
+    if ($pass) {
+        $client = new \GuzzleHttp\Client([
+            'headers' => [
+                'api-key' => env('LETSBUILD_API')
+            ],
+            'base_uri' => 'https://letsbuild.gg/api/'
+        ]);
+
+        $readme = file_get_contents("https://raw.githubusercontent.com/1e4/awesome-letsbuild/master/README.md");
+
+        $post = $readme;
+
+        $time = new \Carbon\Carbon();
+        $post .= "\n\n\nLast synced at {$time} from [Github](https://github.com/1e4/awesome-letsbuild)\n";
+        $post .= "To contribute to the list, open a pull request and if accepted it will get merged and this post will be automatically updated";
+
+        $client->put('articles/16', [
+            'json' => [
+                'article' => [
+                    'body_markdown' => $post
+                ]
+            ]
+        ]);
+
+    }
+});
+
 Route::get('/extensions', function() {
     return \App\Extensions::all();
 });
